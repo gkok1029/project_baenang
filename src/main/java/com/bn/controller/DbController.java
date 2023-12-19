4730ce7bbaf186243948439bc4ae0c5c8b6866fd
UKHWAN LIM <dladnrghks22@gmail.com>
2023-12-19 11:48:08 +0900

    2023-12-19
    1. 중복코드 제거 in PlanMapper.xml
    2. 공백제거 in DbController.java

--- src/main/java/com/bn/controller/DbController.java
+++ src/main/java/com/bn/controller/DbController.java
@@ -1,14 +1,5 @@
 package com.bn.controller;
 
-<<<<<<< HEAD
-
-
-
-
-
-
-=======
->>>>>>> feat.plan
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
