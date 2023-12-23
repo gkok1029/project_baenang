c303cb17575c9e391f8bc6f8a1a44b3f536dc5fb
Heckel <kkwkkw100@gmail.com>
2023-12-23 10:03:00 +0900

    수정 plancontroller
    api호출시도

--- src/main/java/com/bn/controller/PlanController.java
+++ src/main/java/com/bn/controller/PlanController.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -157,7 +158,7 @@ public class PlanController {
 		cd.put("cat", cat);
 		List<ContentVo>nd=dService.searchInRange(cd);
 		map.addAttribute("contentList",nd);
-		System.out.println("controller:" +nd.size());
+		System.out.println(nd.toString());
 		//cd.put("contentList.get(ContentVo).size", map.get(ContentVo));
 		}catch (NumberFormatException e) {
 	        // 숫자 변환 중 예외 발생 시 처리
@@ -195,6 +196,101 @@ public class PlanController {
     	int n=pservice.insertdp(vo);
     	
     	return n;
+    }
+    @ResponseBody
+    @RequestMapping("/makeschedule")
+    public String[] makeschedule() {
+    	String[] result={"126.4946032358,33.2620894948","126.7640945189,33.4734065616", "126.5238756154,33.5118484611",
+                "126.5867476162,33.5255451704", "126.3809026350,33.4810246035"};
+    	 //List<List<String>> permutations = generateAllPermutations(xy);
+    	System.out.println("ㅎㅇ");
+    	 toselectschedule("126.4946032358,33.2620894948","126.7640945189,33.4734065616");
+		System.out.println("gd");	
+    	 /*
+			 * for (List<String> permutation : permutations) { // 각 순열에서 mapx와 mapy 값을 분리하여
+			 * 출력 for (String destination : permutation) { String[] parts =
+			 * destination.split(", "); toselectschedule(parts[0],parts[1]);
+			 * toselectschedule(parts[1],parts[2]); toselectschedule(parts[2],parts[3]);
+			 * toselectschedule(parts[3],parts[4]);
+			 * 
+			 * }
+			 * 
+			 * }
+			 */
+     
+    	
+    	return result;
+    }
+    public static List<List<String>> generateAllPermutations(String[] destinations) {
+        List<List<String>> result = new ArrayList<>();
+        generatePermutations(destinations, 0, result);
+        return result;
+    }
+    private static void generatePermutations(String[] destinations, int currentIndex, List<List<String>> result) {
+        if (currentIndex == destinations.length - 1) {
+            // 배열의 끝에 도달하면 현재 상태를 결과에 추가
+            List<String> permutation = new ArrayList<>();
+            for (String destination : destinations) {
+                permutation.add(destination);
+            }
+            result.add(permutation);
+            return;
+        }
+
+        for (int i = currentIndex; i < destinations.length; i++) {
+            // 배열의 각 요소와 현재 위치의 요소를 교환하지 않고 그대로 사용
+            // 재귀적으로 다음 위치의 순열을 생성
+            generatePermutations(destinations, currentIndex + 1, result);
+        }
+    }
+    public int toselectschedule(String part1,String part2){
+    	try {
+    		System.out.println("part1:"+part1);
+    		System.out.println("part2:"+part2);
+			String text = URLEncoder.encode(part1, "UTF-8");
+			String text2 = URLEncoder.encode(part2, "UTF-8");
+    		System.out.println("ㅎㅇ1");
+			String apiURL = "https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start={"+part1+"}&goal={"+part2+"}";
+			System.out.println(apiURL);
+			URL url = new URL(apiURL);
+			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			conn.setUseCaches(false);
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setRequestProperty("Content-Type", "text/plain");
+			conn.setRequestProperty("X-NCP-APIGW-API-KEY-ID", NAVER_MAPS_KEY);
+			conn.setRequestProperty("X-NCP-APIGW-API-KEY", NAVER_MAPS_SECRET_KEY);
+			int responseCode = conn.getResponseCode();
+			System.out.println(responseCode);
+			if (responseCode == 200) {
+				BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+				String inputLine;
+				StringBuilder response = new StringBuilder();
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+
+				JsonObject jsonResponse = JsonParser.parseString(response.toString()).getAsJsonObject();
+				System.out.println("gd2");
+				System.out.println(jsonResponse.toString());
+				JsonArray addresses = jsonResponse.getAsJsonArray("addresses");
+
+				if (addresses.size() > 0) {
+					JsonObject firstAddress = addresses.get(0).getAsJsonObject();
+					String x = firstAddress.get("x").getAsString();
+					String y = firstAddress.get("y").getAsString();
+					
+				} else {
+					
+				}
+			}
+		} catch (Exception e) {
+			log.error("search error: "+e);
+		}
+    	
+    	
+    return 1;	
     }
     
 }
