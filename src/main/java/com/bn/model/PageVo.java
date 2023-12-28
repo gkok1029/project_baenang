0698245b1c43b6886e2f39e3e3ae1629598eb843
BaekMinGeun <gkok1029@gmail.com>
2023-12-28 23:04:09 +0900

    사용자와 관련된 로직인 MemberService , MemberMapper , MemberController , 구현 ( 로그인 , 회원가입 , 마이페이지에서의 수정 ,삭제 , 스크롤로 페이지 넘기는 처리 등 모든기능구현)

---
+++ src/main/java/com/bn/model/PageVo.java
@@ -0,0 +1,36 @@
+package com.bn.model;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PageVo {
+	private int p_id;
+	private String p_name;
+	private String p_birth;
+	private Date p_moddate;
+	private Date dp_start;
+	private Date dp_end;
+	private long m_id;
+	private String m_email;
+	private String m_pwd;
+	private String m_gen;
+	private String m_nname;
+	private Date m_birth;
+	private Date m_rdate;
+	private String m_image;
+	private int m_status;
+	private int m_profil;
+	private String filter;
+	private long i_id;
+	private int i_per;
+	private int i_status;
+	private String filename;
+	private String originFilename;
+	private long filesize;
+}
